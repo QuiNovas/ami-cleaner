a3707a5f13397d723035f5908cce5ce1d0369843
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-06-21 17:12:17 +0530

    added py files

--- src/lambda_function/function.py
+++ src/lambda_function/function.py
@@ -2,20 +2,81 @@ import boto3
 import logging
 import os
 import time
+import json
+import datetime
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+ec2_client = boto3.client('ec2')
+autoscaling_client = boto3.client('autoscaling')
+
+OLDER_THAN_DAYS = os.environ['OLDER_THAN_DAYS']
+
+launch_configurations_dump = autoscaling_client.describe_launch_configurations()
+launch_templates_dump = ec2_client.describe_launch_templates()
+ami_info_dump = ec2_client.describe_images( Owners = ['self'] )
 
 def handler(event, context):
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+    response = ami_list(ami_info_dump)
+	#Response contains AMI ID's and there corresponding Snapshots which are removed
+    return response
 
-    inspector = boto3.client('inspector')
+def ami_list(ami_list):
+    response = []
+    for items in ami_list['Images']:
+        is_expired = expire(items['ImageId'], items['CreationDate'])
+        if is_expired != None : 
+            response.append(is_expired)
+    return response	
+	
+	    
+def expire(ami_id, creation_date):
+    ami_ids = []
+    date_now = datetime.datetime.now().date()
+    creation_date_only = datetime.datetime.strptime(creation_date, "%Y-%m-%dT%H:%M:%S.%fZ").date()
 
-    logger.info('Starting Inspector run {}'.format(os.environ['ASSESSMENT_TEMPLATE_ARN']))
-    run_name = '{}{}'.format(os.environ.get('RUN_NAME_PREFIX', ''), time.strftime('%Y-%m-%d %H:%M:%S'))
-    inspector.start_assessment_run(
-        assessmentTemplateArn=os.environ['ASSESSMENT_TEMPLATE_ARN'],
-        assessmentRunName=run_name
-    )
-    logger.info('Started Inspector run {}'.format(run_name))
-
-    return event
+    if (date_now - creation_date_only).days > int(OLDER_THAN_DAYS):
+        if(check_launch_configurations(ami_id) and check_launch_templates(ami_id)):
+            logger.info('Will Deregister AMI {}'.format(ami_id))
+            ami_ids.append(ami_id)
+            deregister_data = deregister_image(ami_id)
+            return ami_ids, deregister_data
+        
+def check_launch_configurations(ami_id):
+    for items in launch_configurations_dump['LaunchConfigurations']:
+        if ami_id == items['ImageId']:
+            logger.info("{} Getting used in launch configurations, Will not be removed.".format(ami_id))
+            return False
+    return True
+    
+def check_launch_templates(ami_id):
+    for items in launch_templates_dump['LaunchTemplates']:
+	    if not (check_launch_template_versions(items['LaunchTemplateId'], ami_id)):
+		    logger.info("{} Getting used in one or more launch template versions, Will not be removed.".format(ami_id))
+		    return False
+    return True
+	
+def check_launch_template_versions(launch_template_id, ami_id):
+    response = ec2_client.describe_launch_template_versions(
+	    LaunchTemplateId=launch_template_id)
+    for items in response['LaunchTemplateVersions']:
+        if ami_id == items['LaunchTemplateData']['ImageId']:            
+            return False
+    return True
+	
+def deregister_image(ami_id):
+	snapshots = []
+	
+	#Get the SnapshotIDs before deleting the AMI.
+	for items in ami_info_dump['Images']:
+	    if ami_id == items['ImageId']:
+		    for blockDeviceMappings in items['BlockDeviceMappings']:
+			    snapshots.append(blockDeviceMappings['Ebs']['SnapshotId'])
+	#Deregistering AMI			
+	logger.info("Removing AMI {}".format(ec2_client.deregister_image(ImageId=ami_id)))
+	
+	#Deleting all ebs snapshots of the AMI
+	for items in snapshots:
+	    logger.info("Removing snapshot {}".format(ec2_client.delete_snapshot(SnapshotId=items)))
+	return snapshots
